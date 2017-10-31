36634a5ce70a6f9ddf65eae842a76f0e054b3108
thangpham <wins1908@gmail.com>
2017-10-31 16:23:01 +0700

    Fixed issue `db.Dialect undefined`
    db.Dialect() is gone, use os.Getenv("GORM_DIALECT") instead

--- nrgorm/gorm.go
+++ nrgorm/gorm.go
@@ -2,6 +2,7 @@ package nrgorm
 
 import (
 	"fmt"
+	"os"
 	"strings"
 
 	"github.com/jinzhu/gorm"
@@ -23,7 +24,7 @@ func SetTxnToGorm(txn newrelic.Transaction, db *gorm.DB) *gorm.DB {
 
 // AddGormCallbacks adds callbacks to NewRelic, you should call SetTxnToGorm to make them work
 func AddGormCallbacks(db *gorm.DB) {
-	dialect := db.Dialect().GetName()
+	dialect := os.Getenv("GORM_DIALECT")
 	var product newrelic.DatastoreProduct
 	switch dialect {
 	case "postgres":
